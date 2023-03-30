fc09a2b7532993e10447d7cf8baa8b2e6905fb33
Asitha Nanayakkara <daun07@gmail.com>
2023-03-30 16:04:28 +0530

    Add multiport service sample

---
+++ go/multiport-service/go.mod
@@ -0,0 +1,3 @@
+module github.com/wso2/choreo-sample-apps/go/multiport-service
+
+go 1.19
---
+++ go/multiport-service/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	serverMux8080 := http.NewServeMux()
+	serverMux6657 := http.NewServeMux()
+	serverMux9876 := http.NewServeMux()
+	serverMux9090 := http.NewServeMux()
+
+	serverMux8080.HandleFunc("/greeting8080/hello", hello8080)
+	serverMux6657.HandleFunc("/greeting6657/hello", hello6657)
+	serverMux9876.HandleFunc("/greeting9876/hello", hello9876)
+	serverMux9090.HandleFunc("/greeting9090/hello", hello9090)
+
+	log.Println("Starting server...")
+
+	go func() {
+		check(http.ListenAndServe(":6657", serverMux6657))
+	}()
+
+	go func() {
+		check(http.ListenAndServe(":8080", serverMux8080))
+	}()
+
+	go func() {
+		check(http.ListenAndServe(":9876", serverMux9876))
+	}()
+
+	check(http.ListenAndServe(":9090", serverMux9090))
+
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func hello8080(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello %s, from port 8080", r.URL.Query().Get("name"))
+}
+
+func hello6657(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello %s, from port 6657", r.URL.Query().Get("name"))
+}
+
+func hello9876(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello %s, from port 9876", r.URL.Query().Get("name"))
+}
+
+func hello9090(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello %s, from port 9090", r.URL.Query().Get("name"))
+}
